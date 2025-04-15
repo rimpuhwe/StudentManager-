c72f4a26ee198546f92bb9e33abb66a030d441a6
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-15 16:12:51 +0200

    creating the studentManager using hashMap

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private String name;
+    private int id;
+
+
+//  Creating the constructor
+    Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ src/StudentManager.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class StudentManager {
+
+    public StudentManager() {
+        // Constructor to initialize the HashMap
+        HashMap<Student, String> studentMap = new HashMap<>();
+
+    }
+}
