dac7d984715322508518875064ee6adce0f9a4d0
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-16 11:07:26 +0200

    creating a list to hold the student objects

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,9 +7,15 @@ public class Main {
         student1.addStudent("Anne" , 123 , "java");
         StudentManager student2 = new StudentManager();
         student2.addStudent("Carine" , 122 , "Python");
+        ArrayList<StudentManager> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        System.out.println(students);
+        StudentManager.sortStudents(students);
 
-        System.out.println(student1.getStudentMap());
-        System.out.println(student2.getStudentMap());
+
+//        System.out.println(student1.getStudentMap());
+//        System.out.println(student2.getStudentMap());
 
 
     }
