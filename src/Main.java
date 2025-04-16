e0146481409166a7580a11d389ea223c8fabddb8
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-16 09:22:04 +0200

    update the studentManager

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+        StudentManager student1 = new StudentManager();
+        student1.addStudent("Anne" , 123 , "java");
+        StudentManager student2 = new StudentManager();
+        student2.addStudent("Carine" , 122 , "Python");
+
+        System.out.println(student1.getStudentMap());
+        System.out.println(student2.getStudentMap());
+
 
     }
 }
--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,10 +1,23 @@
 import java.util.HashMap;
 
-public class StudentManager {
-
-    public StudentManager() {
-        // Constructor to initialize the HashMap
-        HashMap<Student, String> studentMap = new HashMap<>();
-
+public class StudentManager{
+    private final HashMap<String , Object> studentMap;
+    // building constructor
+    StudentManager(){
+        this.studentMap = new HashMap<>();
     }
+
+    // method for adding the student
+    public void addStudent(String name , int id , String course) {
+        studentMap.put("name", name);
+        studentMap.put("id", id);
+        studentMap.put("course", course);
+    }
+    //method for returning the map
+    public HashMap<String , Object> getStudentMap(){
+        return studentMap;
+    }
+
+
+
 }
